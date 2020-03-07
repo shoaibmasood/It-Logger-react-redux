e0f21f261dcf8ba5c3b83479233dc6a3b284010f
Shoaib <shoaib_masood@rocketmail.com>
2020-03-07 14:58:38 +0500

    added getlog and addlog feature

---
+++ src/actions/logActions.js
@@ -0,0 +1,69 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+
+// export const getLogs = () => {
+//     return  async (dispatch) => {
+//         setLoading();
+
+//         const res  =await fetch ("./logs");
+//         const data = await res.json();
+
+//         dispatch({
+//             type: GET_LOGS,
+//             payload: data
+//         });
+
+//     }
+// };
+
+//Get Logs fro server
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('./logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// Add new Log
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('./logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+//Set Loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
