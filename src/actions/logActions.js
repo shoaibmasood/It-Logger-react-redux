047b6c1c8800f2fffa29a11e374c1e9375cba87c
Shoaib <shoaib_masood@rocketmail.com>
2020-03-09 02:04:28 +0500

    added log updated Features

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG
 } from './types';
 
 // export const getLogs = () => {
@@ -86,6 +89,48 @@ export const deleteLog = id => async dispatch => {
       payload: err.response.data
     });
   }
+};
+
+// Update log on Server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`./logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// Set Current log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+};
+
+// clear Current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
 };
 
 //Set Loading to true
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from '../actions/types';
 
 const initialState = {
@@ -36,6 +39,24 @@ export default (state = initialState, action) => {
         ...state,
         logs: state.logs.filter(log => log.id !== action.payload),
         loading: false
+      };
+
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map(log =>
+          log.id === action.payload ? action.payload : log
+        )
       };
 
     case SET_LOADING:
