75b19ea0fadc9e7d38ddbbc4e34fc739e31be8ea
Shoaib <shoaib_masood@rocketmail.com>
2020-03-09 03:12:37 +0500

    added TechSelectOptions component

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
-
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 const AddLogModal = ({ addLog }) => {
@@ -61,9 +61,7 @@ const AddLogModal = ({ addLog }) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith"> Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
