8018aca7a9d8784a93ae7d11b522ed76a0de7eee
Shoaib <shoaib_masood@rocketmail.com>
2020-03-09 02:25:11 +0500

    added Searchlog Features

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => {
+import { searchLogs } from '../../actions/logActions';
+
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
+
+  const onChange = e => {
+    searchLogs(text.current.value);
+  };
   return (
     <nav style={{ marginBottom: '30px' }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" required />
+            <input
+              id="search"
+              type="search"
+              placeholder="Search Logs..."
+              ref={text}
+              onChange={onChange}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
@@ -17,5 +32,8 @@ const SearchBar = () => {
     </nav>
   );
 };
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired
+};
 
-export default SearchBar;
+export default connect(null, { searchLogs })(SearchBar);
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   UPDATE_LOG,
   SET_CURRENT,
-  CLEAR_CURRENT
+  CLEAR_CURRENT,
+  SEARCH_LOGS
 } from '../actions/types';
 
 const initialState = {
@@ -57,6 +58,12 @@ export default (state = initialState, action) => {
         logs: state.logs.map(log =>
           log.id === action.payload ? action.payload : log
         )
+      };
+
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
       };
 
     case SET_LOADING:
